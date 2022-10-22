cc342b726ae3e1523c3bb720dd5635278a0cf327
KatyaLivintseva <113354368+KatyaLivintseva@users.noreply.github.com>
2022-10-22 10:03:04 +0500

    код переделан

--- Shifr.py
+++ Shifr.py
@@ -1,70 +1,7 @@
-alfavit='абвгдеёжзийклмнопрстуфхцчшщъыьэюя' #задаем алфавит
+alfavit='абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя' #задаем алфавит
 print('\nЭто "Шифр Цезаря". Он поможет зашифровать или расшифровать послание')
-print('\nНапишите текст (на русском языке), который нужно зашифровать или расшифровать')
-try:
-    A=0
-    text=input()
-    print('Сдвиг шифровки')
-
-    try:
-        sdvig=int(input())
-        text_z=text.lower() 
-        text_new=''
-        
-        if sdvig==0:
-            print("Ваш текст не изменился:", text)
-         
-        if sdvig>=1: #зашифровка
-            while sdvig > 33: #если сдвиг больше букв в алфавите
-                sdvig -= 33
-            k = sdvig
-            
-            for ic in text_z:
-                position=alfavit.find(ic)
-                new_position=position+k 
-                if ic in alfavit:
-                    text_new=text_new + alfavit[new_position]
-                else:
-                    text_new=text_new+ic
-                    
-            if text==text_new:
-                A=1
-                print('Вы ввели или символ, или текст не на русском языке')
-            else:
-                print ('Зашифрованный текст:', text_new)
-                
-        if sdvig<0: #расшифровка
-            while sdvig <(-33):
-                sdvig += 33
-            k = sdvig
-            
-            for ic in text_z:
-                position=alfavit.find(ic)
-                new_position=position+k
-                if ic in alfavit:
-                    text_new=text_new + alfavit[new_position]
-                else:
-                    text_new=text_new+ic
-                    
-            if text==text_new:
-                A=1
-                print('Вы ввели или символ, или текст не на русском языке')
-            else:
-                print ('Расшифрованный текст:', text_new)    
-        
-    except IndexError:
-        A=1
-        print('Шаг сдвига слишком большой.  ')
-
-    except ValueError:
-        A=1
-        print('Шаг должен быть обозначен цифрой.')
-
-except TypeError:
-    A=1
-    print('Введите текст.')
-    
-while A==1:
+def shifr():
+    global A
     print('\nНапишите текст (на русском языке), который нужно зашифровать или расшифровать')
     try:
         A=0
@@ -75,32 +12,34 @@ while A==1:
             sdvig=int(input())
             text_z=text.lower() 
             text_new=''
-            
+
             if sdvig==0:
                 print("Ваш текст не изменился:", text)
-             
-            if sdvig>=1:
-                while sdvig > 33:
+
+            if sdvig>=1: #зашифровка
+                while sdvig > 33: #если сдвиг больше букв в алфавите
                     sdvig -= 33
                 k = sdvig
+
                 for ic in text_z:
-                    position=alfavit.find(ic)                    
-                    new_position=position+k
-                    
+                    position=alfavit.find(ic)
+                    new_position=position+k 
                     if ic in alfavit:
                         text_new=text_new + alfavit[new_position]
                     else:
                         text_new=text_new+ic
-                if text==text_new: #если ввели не то, что надо (текст на русском)
+
+                if text==text_new:
                     A=1
                     print('Вы ввели или символ, или текст не на русском языке')
                 else:
                     print ('Зашифрованный текст:', text_new)
-                    
-            if sdvig<0:
+
+            if sdvig<0: #расшифровка
                 while sdvig <(-33):
                     sdvig += 33
                 k = sdvig
+
                 for ic in text_z:
                     position=alfavit.find(ic)
                     new_position=position+k
@@ -108,12 +47,13 @@ while A==1:
                         text_new=text_new + alfavit[new_position]
                     else:
                         text_new=text_new+ic
+
                 if text==text_new:
                     A=1
                     print('Вы ввели или символ, или текст не на русском языке')
                 else:
                     print ('Расшифрованный текст:', text_new)    
-            
+
         except IndexError:
             A=1
             print('Шаг сдвига слишком большой.  ')
@@ -125,3 +65,8 @@ while A==1:
     except TypeError:
         A=1
         print('Введите текст.')
+A=0
+if A==0:
+    shifr()
+while A==1:
+    shifr()
